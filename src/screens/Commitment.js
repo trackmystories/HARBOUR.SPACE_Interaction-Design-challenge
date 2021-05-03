c5ccfa14455933775b33419c6d48b99c2815aeb2
trackmystories <ashirazee@gmail.com>
2021-05-03 08:25:34 +0500

    03_05_21

--- src/screens/Commitment.js
+++ src/screens/Commitment.js
@@ -7,33 +7,33 @@ import "./styles/commitment.css";
 
 export default function Commitment() {
   return (
-    <div is="commitment__section">
-      <div className="commitment__section-flex">
-        <ScholarshipValue />
-        <div className="marginRight" />
-        <div>
-          <div className="commitment__section-flex-2Boxes">
-            <StudyWork
-              title="Study Commitment"
-              hours="3 hours / day"
-              desc="You will complete 15 modules to graduate. Daily classes are 3 hours, plus coursework to complete in your own time."
-            />
+    <div id="commitment__section">
+      <div className="commitment">
+        <div className="commitment__section-flex">
+          <ScholarshipValue />
+          <div className="marginRight" />
+          <div>
+            <div className="commitment__section-flex-2Boxes">
+              <StudyWork
+                title="Study Commitment"
+                hours="3 hours / day"
+                desc="You will complete 15 modules to graduate. Daily classes are 3 hours, plus coursework to complete in your own time."
+              />
 
-            <StudyWork
-              title="Work Commitment"
-              hours="4 hours / day"
-              desc="Immerse yourself in the professional world during your apprenticeship. You’ll learn the ropes from the best and get to apply your newly acquired knowledge in the field from day one.  "
-            />
-          </div>
+              <StudyWork
+                title="Work Commitment"
+                hours="4 hours / day"
+                desc="Immerse yourself in the professional world during your apprenticeship. You’ll learn the ropes from the best and get to apply your newly acquired knowledge in the field from day one.  "
+              />
+            </div>
 
-          <div className="marginBottom" />
+            <div className="marginBottom" />
 
-          <div className="commitment__section-fulltime-contract">
-            <FullTimeContract />
+            <div className="commitment__section-fulltime-contract">
+              <FullTimeContract />
+            </div>
           </div>
         </div>
-
-        <div></div>
       </div>
     </div>
   );
